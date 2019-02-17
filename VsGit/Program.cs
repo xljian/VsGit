b9e784f3caa0452e08c6576bfbd7ab53a4c56fbe
xljian <978298528@qq.com>
2019-02-17 23:28:59 +0800

    添加项目文件。

---
+++ VsGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VsGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
